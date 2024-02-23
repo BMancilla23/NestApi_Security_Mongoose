f3d5b5125eba989771e0df4ce09ec5fafe94083e
bmancilla23 <bmancilla119@gmail.com>
2024-02-23 03:12:35 -0500

    Definición de ambientes de entornos

--- src/common/environments.ts
+++ src/common/environments.ts
@@ -1,5 +1,8 @@
+// Definimos un objeto llamado `environments` que contiene las rutas a los archivos de configuración de entorno
 export const environments = {
+    // En el entorno de desarrollo (dev), el archivo de configuración se llama ".dev.env"
     dev: ".dev.env",
+    // En el entorno de producción (prod), el archivo de configuración se llama ".prod.env"
     prod: ".prod.env"
 };
 
