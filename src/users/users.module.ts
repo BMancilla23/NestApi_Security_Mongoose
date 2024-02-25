4efe695bc80c639b901927dfee0f9cca04efadeb
bmancilla23 <bmancilla119@gmail.com>
2024-02-25 05:27:52 -0500

    Configuracion de proveedores de hashing

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,16 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './entities/user.entity';
+import { HashingService } from 'src/providers/hashing/hashing.service';
+import { BcryptService } from 'src/providers/hashing/bcrypt.service';
 
 @Module({
   imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [
+    {provide: HashingService, // El token del proveedor es la clase abstracta
+     useClass: BcryptService // Se especifica la implementación concreta
+    }, UsersService],
   exports: [UsersService]
 })
 export class UsersModule {}
